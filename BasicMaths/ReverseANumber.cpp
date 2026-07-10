954932e2aa1fb0408450ecb718b0aad1d0a66ee4
Krishna Vatanevakil <krishna.vatanevakil.1892@gmail.com>
2026-07-10 16:32:24 +0530

    Create ReverseANumber.cpp

---
+++ BasicMaths/ReverseANumber.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    while(n > 0){
+        int ld = n % 10;
+        ans = ans * 10 + ld;
+        n = n / 10;
+    }
+    cout << ans << endl;
+}
